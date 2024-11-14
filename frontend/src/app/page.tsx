e2667eae25fd650a5fe3aa67d8501f10dae6941c
MertAygunn <mertaygun97@gmail.com>
2024-11-14 23:24:54 +0300

    main update

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+// src/app/page.tsx
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Merhaba, Tailwind CSS!
+      </h1>
+    </div>
+  );
+}
