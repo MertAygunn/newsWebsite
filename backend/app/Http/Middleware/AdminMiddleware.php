0bfd505ceea413f8f6c14d32ff4ff425a0b2cb38
MertAygunn <mertaygun97@gmail.com>
2024-11-16 18:21:51 +0300

    commit

--- backend/app/Http/Middleware/AdminMiddleware.php
+++ backend/app/Http/Middleware/AdminMiddleware.php
@@ -9,10 +9,16 @@ class AdminMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
-        if (auth()->user() && auth()->user()->role === 'admin') {
-            return $next($request);
+        // Kullanıcının kimlik doğrulamasını kontrol et
+        if (!auth('sanctum')->check()) {
+            return response()->json(['message' => 'Unauthorized access.'], 403);
         }
 
-        return response()->json(['message' => 'Yetkisiz erişim.'], 403);
+        // Kullanıcının rolünü kontrol et
+        if (auth('sanctum')->user()->role !== 'admin') {
+            return response()->json(['message' => 'Forbidden access. You do not have the required permissions.'], 403);
+        }
+
+        return $next($request);
     }
 }
